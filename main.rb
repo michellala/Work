5ac658030ba2e028666cea9e5a1f4e5c16c4653a
Michelle Alarie <alarie.michelle@gmail.com>
2016-04-29 17:46:38 -0400

    Candidates stuff

---
+++ main.rb
@@ -0,0 +1,34 @@
+# This is the main entrypoint into the program
+# It requires the other files/gems that it needs
+
+require 'pry'
+require './candidates'
+require './filters'
+
+## Your test code can go here
+
+
+#tests
+#puts experienced?(@candidates[0])
+
+#experienced?(@candidates[1]) 
+
+#puts find(9)
+
+
+#p can_code?(@candidates[0])
+
+#test
+#puts find(@candidates,(10))
+#option 2: puts find (10)
+
+
+
+#test
+#p date_of_application?(@candidates[0])
+
+#pp qualified_candidates(@candidates)
+
+#pp ordered_by_qualifications?(@candidates)
+
+pp menu 
